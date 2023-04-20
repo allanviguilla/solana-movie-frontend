644163bba45e25be8ecb1d0e6630f427717d23e3
allanviguilla <viguilla.allan@gmail.com>
2023-04-20 11:13:20 -0700

    Complete handleTransactionSubmit function

--- models/Movie.ts
+++ models/Movie.ts
@@ -41,7 +41,7 @@ export class Movie {
     borsh.str("description"),
   ]);
 
-  seralize(): Buffer {
+  serialize(): Buffer {
     const buffer = Buffer.alloc(1000);
     this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer);
     return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer));
